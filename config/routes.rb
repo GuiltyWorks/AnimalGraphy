8f35aab431f03ce2398024a0ea9bd959cd75311c
GuiltyWorks <GuiltyWorks@protonmail.com>
2020-06-20 16:43:42 +0900

    Improve not being able to edit replies

--- config/routes.rb
+++ config/routes.rb
@@ -10,39 +10,23 @@ Rails.application.routes.draw do
     omniauth_callbacks: "omniauth_callbacks",
   }
 
-  get "/zoo" => "zoo#index"
-
-  get "/news/:key" => "news#index"
-
-  resources :replies, only: [ :edit, :create, :update, :destroy ]
-  # get "replies/:id/edit" => "replies#edit"
-  # post "replies/:post_id/create" => "replies#create"
-  # post "replies/:id/update" => "replies#update"
-  # post "replies/:id/destroy" => "replies#destroy"
-
-  post "likes/:post_id/create" => "likes#create"
-  post "likes/:post_id/destroy" => "likes#destroy"
-
-  post "login" => "users#login"
-  post "logout" => "users#logout"
+  get "/" => "posts#index"
+  get "/about" => "home#about"
 
   get "users/index" => "users#index"
   get "users/:id/likes" => "users#likes"
   get "users/:id" => "users#show"
 
-  # get "posts/index" => "posts#index"
   get "posts/search" => "posts#search"
   get "posts/ranking/:period" => "posts#ranking"
-  # get "posts/new" => "posts#new"
   get "posts/tags/:id" => "posts#tags"
   resources :posts
-  # get "posts/:id/edit" => "posts#edit"
-  # get "posts/:id" => "posts#show"
 
-  # post "posts/create" => "posts#create"
-  # post "posts/:id/update" => "posts#update"
-  # post "posts/:id/destroy" => "posts#destroy"
+  resources :likes, only: [ :create, :destroy ]
 
-  get "/" => "posts#index"
-  get "/about" => "home#about"
+  resources :replies, only: [ :edit, :create, :update, :destroy ]
+
+  get "/news/:key" => "news#index"
+
+  get "/zoo" => "zoo#index"
 end
